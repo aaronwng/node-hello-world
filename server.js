70961665b3ae2541eec63a91e132088f47bf07ee
Aaron Wang <arronwlsnn@gmail.com>
2019-07-08 20:08:31 +0800

    update server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function(request, response){
     console.log(request.method, request.url)
     response.writeHead(200, { 'Content-Type': 'text-plain' });
-    response.end('Hello World\n');
+    response.end('Hello CD\n');
 }).listen(8080);
